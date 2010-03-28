731de09f0830e67a4ef5999f56d6172089274efa
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2010-03-28 00:56:57 +0000

    0.01
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/RandomWord/trunk@144 adbff654-c4ea-448a-8ae3-cad037306009

---
+++ RandomWord.pl
@@ -0,0 +1,43 @@
+package MT::Plugin::OMV::RandomWord;
+
+use strict;
+use MT 3;
+use MT::Template::Context;
+
+use vars qw( $MYNAME $VERSION );
+$MYNAME = 'RandomWord';
+$VERSION = '0.01';
+
+use base qw( MT::Plugin );
+my $plugin = __PACKAGE__->new({
+    id => $MYNAME,
+    key => $MYNAME,
+    name => $MYNAME,
+    version => $VERSION,
+    author_name => 'Open MagicVox.net',
+    author_link => 'http://www.magicvox.net/',
+    doc_link => '',
+    description => <<PERLHEREDOC,
+Return a word randomly from the comma separated words list.
+PERLHEREDOC
+});
+MT->add_plugin ($plugin);
+
+sub instance { $plugin; }
+
+
+
+###
+MT::Template::Context->add_container_tag ($MYNAME => sub {
+    my ($ctx, $args, $cond) = @_;
+
+    defined (my $out = $ctx->slurp ($args, $cond))
+        or return;
+
+    my $delim = $args->{delim} || $args->{glue} || ',';
+    my @out = split $delim, $out;
+
+    $out[int (rand ($#out))];
+});
+
+1;
